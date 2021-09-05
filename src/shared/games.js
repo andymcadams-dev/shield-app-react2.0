32ca49a19fa67722b07861b46d9eb64dcf380d8b
Andy McAdams <andymcadams-dev@gmail.com>
2021-09-05 07:49:30 -0500

    team logos

---
+++ src/shared/games.js
@@ -0,0 +1,92 @@
+export const GAMES = [
+    {
+        id: 0,
+        school: "California Lutheran University",
+        image: "/assets/CalLutheranLogo.png",
+        featured: false,
+        date: "9/11/21 (SAT) 7pm",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 1,
+        name: "University of La Verne",
+        image: "/assets/LaVerneLogo.png",
+        featured: false,
+        date: "9/18/21 (SAT) 12:30pm",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 2,
+        name: "George Fox University",
+        image: "/assets/GeorgeFoxLogo.png",
+        featured: false,
+        date: "9/18/21 (SAT) 12:30pm",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 3,
+        name: "Lewis & Clark University",
+        image: "/assets/LewisNClarkLogo.png",
+        featured: true,
+        date: "10/9/21 (SAT) 1pm",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 4,
+        name: "Whitworth University",
+        image: "/assets/WhitworthLogo.png",
+        featured: true,
+        date: "10/16/21 (SAT) TBA",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 5,
+        name: "Linfield University",
+        image: "/assets/LinfieldLogo.png",
+        featured: true,
+        date: "10/23/21 (SAT) TBA",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 6,
+        name: "Willamette University",
+        image: "/assets/WillametteLogo.png",
+        featured: true,
+        date: "10/30/21 (SAT) TBA",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 7,
+        name: "UNiversity of Puget Sound",
+        image: "/assets/PugetSoundLogo.png",
+        featured: true,
+        date: "11/6/21 (SAT) 1pm",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    },
+    {
+        id: 8,
+        name: "Pacific University",
+        image: "/assets/PacificLogo.png",
+        featured: true,
+        date: "11/13/21 (SAT) TBA",
+        score: "N/A",
+        winloss: "N/A",
+        teamgrade: "N/A"
+    }
+]
